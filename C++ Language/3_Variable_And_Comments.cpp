acab5fb9e5386bb75bc8796c7eaf50d47b5d849b
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-07-27 13:16:05 +0530

    Add files via upload

---
+++ C++ Language/3_Variable_And_Comments.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int sum = 6;
+    cout<<"hello world "<<sum;
+    // this is single line comment
+    /*
+    this
+    is
+    multi
+    line
+    comment
+    */
+    return 0;
+}
