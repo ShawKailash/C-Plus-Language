2f09219c71088d3147f1df06e0b4e89ab7a80eba
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-07-28 14:46:58 +0530

    Add files via upload

---
+++ C++ Language/6_Operator.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=8, b=4;
+    cout<<"operator in c++ "<<endl;
+    cout<<"the value of a + b is "<<a+b<<endl;
+    cout<<"the value of a - b is "<<a-b<<endl;
+    cout<<"the value of a * b is "<<a*b<<endl;
+    cout<<"the value of a / b is "<<a/b<<endl;
+    cout<<"the value of a % b is "<<a%b<<endl;
+    cout<<"the value of a++ is "<<a++<<endl;
+    cout<<"the value of a-- is "<<a--<<endl;
+    cout<<"the value of ++a is "<<++a<<endl;
+    cout<<"the value of --a is "<<--a<<endl;
+    cout<<endl;
+    cout<<"the value of a == b is "<<(a==b)<<endl;
+    cout<<"the value of a != b is "<<(a!=b)<<endl;
+    cout<<"the value of a <= b is "<<(a<=b)<<endl;
+    cout<<"the value of a >= b is "<<(a>=b)<<endl;
+    cout<<"the value of a < b is "<<(a<b)<<endl;
+    cout<<"the value of a > b is "<<(a>b)<<endl;
+    cout<<endl;
+    cout<<"the value of this logical and operator ((a==b) && (a>b)) is "<<((a==b) && (a>b))<<endl;
+    cout<<"the value of this logical or operator ((a==b) || (a>b)) is "<<((a==b) || (a>b))<<endl;
+    return 0;
+}
