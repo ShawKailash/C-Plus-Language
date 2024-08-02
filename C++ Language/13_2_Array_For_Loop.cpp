4458c4077995f273d17ce47de9129ad978104dff
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-08-02 11:04:50 +0530

    Add files via upload

---
+++ C++ Language/13_2_Array_For_Loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int marks[]={45,67,89,97};
+    for(int i=0;i<4;i++)
+    {
+        cout<<"the value of marks "<<i<<" is "<<marks[i]<<endl;
+    }
+    return 0;
+}
