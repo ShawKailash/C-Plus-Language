36c936838eb876837e286d145937df7fbebbc320
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-08-02 11:05:44 +0530

    Add files via upload

---
+++ C++ Language/13_3_Array_While_Loop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int marks[]={34,67,87,89};
+    int i=0;
+    while(i<4)
+    {
+        cout<<"the value of marks "<<i<<" is "<<marks[i]<<endl;
+        i++;
+    }
+    return 0;
+}
