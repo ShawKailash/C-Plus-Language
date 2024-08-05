ebaab9192cf75d932c9af679514f52c35e20b9bd
ShawKailash <165937979+ShawKailash@users.noreply.github.com>
2024-08-05 10:49:13 +0530

    Add files via upload

---
+++ C++ Language/14_4_Enums.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    enum meal{breakfast,lunch,dinner};
+    cout<<breakfast<<endl;
+    cout<<lunch<<endl;
+    cout<<dinner<<endl;
+    cout<<endl;
+    meal m1=breakfast;
+    cout<<m1<<endl;
+    return 0;
+}
